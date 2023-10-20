8717f58e6890bc6c12fbb285e91efeb2d6e0bb4e
Bruno Bessa <beduardo2014@gmail.com>
2023-10-20 17:22:16 -0300

    Exception handling - delete

--- src/main/java/com/brunobessa/course/resources/UserResource.java
+++ src/main/java/com/brunobessa/course/resources/UserResource.java
@@ -45,7 +45,7 @@ public class UserResource {
 	}
 	
 	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Void> delete(@PathVariable Long id){
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
--- src/main/java/com/brunobessa/course/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/brunobessa/course/services/exceptions/ResourceNotFoundException.java
@@ -1,10 +1,10 @@
 package com.brunobessa.course.services.exceptions;
 
-public class ResourceNotFoundException extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-	
-	public ResourceNotFoundException(Object id) {
-		super("Resouce not found. Id " + id);
-	}
+public class ResourceNotFoundException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
 }
